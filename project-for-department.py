815807b63f03eca6992de8b58d5ec6d7dce57505
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2023-09-15 21:35:06 +0530

    Add files via upload

---
+++ project-for-department.py
@@ -0,0 +1,16 @@
+emp1=["uma mam",101,"india"]
+emp2=["mr. suresh",103,"america"]
+dep1=["cse",31052,"java"]
+dep2=["It",342,"c++"]
+Hod_it=["mr peeter",20]
+Hod_cse=["malti", 30]
+print("enter the employ detail:")
+print(" emp1:\n Name  : %s, Id  :%d, country:%s,\nemp2:\n Name:%s,Id:%d,country:%s"%
+       (emp1[0],emp1[1],emp1[2],emp2[0],emp2[1],emp2[2]))
+print("enter the departement detail")     
+print("department1:\nName: %s,subject code:%d, subject:%s, \nDepartment2:\n Name:%s,subject code:%d,subject:%s" %
+      
+      (dep1[0],dep1[1],dep1[2],dep2[0],dep2[1],dep2[2]))
+print("enter the Hod detail:-")
+print("Hod it: Name:\n %s,total handle stape:%d, \n Hod Cse \n Name:%s,total handle stape:%d "%
+      (Hod_it[0],Hod_it[1],Hod_cse[0],Hod_cse[1] ))
