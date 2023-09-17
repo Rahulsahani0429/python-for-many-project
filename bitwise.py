b59881d269654c183e8742ac4a2adcf2e38b9d5d
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2023-09-17 21:43:33 +0530

    Add files via upload

---
+++ bitwise.py
@@ -0,0 +1,11 @@
+a=10
+b=7
+# print(a,bin(a))
+# print((a+b))
+print(bin(a))#bin funtion is use to convert the number into the boolean
+print(bin(b))
+# print(a&b)
+# print(a|b)
+print(a&b)#and operator in this operator and means multiplication
+print(a|b)#or  operator
+print(a^b)#xor operator
