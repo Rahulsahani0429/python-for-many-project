d2db7be2c6242df8b98226bbc40fc9862733e078
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2023-09-17 21:45:47 +0530

    Add files via upload

---
+++ mark.py
@@ -0,0 +1,9 @@
+a=int(input('enter the marks:-'))
+if a>=80:
+    print("you passed in first division ")
+elif a>=60:
+    print('you passed in second division')
+elif a>=40:
+    print('you passed in third division')
+else:
+    print('fail')
